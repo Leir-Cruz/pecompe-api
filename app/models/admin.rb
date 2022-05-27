09e878670f372cd5e6a17c42a71e9332c43f99fd
Leir-Cruz <gabrielcruzvazsantos@gmail.com>
2022-05-27 11:37:37 -0300

    :sparkles: admin fields

---
+++ app/models/admin.rb
@@ -0,0 +1,11 @@
+class Admin < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates_presence_of :username
+  validates_uniqueness_of :username
+  validates_length_of :username, minimum: 4
+
+end
